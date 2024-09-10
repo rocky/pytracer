6860542ca055965d831104c8c576258b1354b372
rocky <rocky@gnu.org>
2024-09-09 21:30:11 -0400

    First cut at version segmentations

---
+++ admin-tools/setup-python-3.6.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Check out 3.6-to-3.10 branch and dependent development branches
+s
+bs=${BASH_SOURCE[0]}
+if [[ $0 == $bs ]] ; then
+    echo "This script should be *sourced* rather than run directly through bash"
+    exit 1
+fi
+
+PYTHON_VERSION=3.6
+
+export PATH=$HOME/.pyenv/bin/pyenv:$PATH
+pytracer_owd=$(pwd)
+mydir=$(dirname $bs)
+cd $mydir
+. ./checkout_common.sh
+checkout_finish python-3.6-to-3.10
