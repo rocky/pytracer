3fc52af88c10e0515be4933bfd3c118a99f85a57
rocky <rocky@gnu.org>
2024-09-09 22:00:42 -0400

    Revert pyproject.toml stuff

--- setup.py
+++ setup.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python
 """
-setuptools setup (setup.py) for pytracer.
+distutils setup (setup.py) for pytracer.
 
 This gets a bit of package info from __pkginfo__.py file
 """
 # Get the required package information
+from __pkginfo__ import \
+    author,           author_email,       classifiers,                    \
+    license,          long_description,   modname,          package_dir,  \
+    short_desc,       version,            web,              zip_safe
+
 from setuptools import setup
 
-setup(packages=["tracer"])
+import os
+top_dir = os.path.dirname(__file__)
+README  = os.path.join(top_dir, 'README.rst')
+
+# Description in package will come from the README file.
+long_description = open(README).read() + '\n\n'
+
+setup(
+      author             = author,
+      author_email       = author_email,
+      classifiers        = classifiers,
+      description        = short_desc,
+      license            = license,
+      long_description   = long_description,
+      name               = modname,
+      package_dir        = package_dir,
+      py_modules         = ['tracer', 'tracefilter'],
+      test_suite         = 'nose.collector',
+      url                = web,
+      setup_requires     = ['nose>=1.0'],
+      version            = version,
+      zip_safe           = zip_safe
+      )
