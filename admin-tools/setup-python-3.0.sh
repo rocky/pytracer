bcef90e59da625f90bb8df3bdca2dabf70b470db
rocky <rocky@gnu.org>
2024-10-04 22:50:03 -0400

    Adminsitrivia

---
+++ admin-tools/setup-python-3.0.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Check out 3.0-to-3.2 branch and dependant development branches
+
+bs=${BASH_SOURCE[0]}
+if [[ $0 == $bs ]] ; then
+    echo "This script should be *sourced* rather than run directly through bash"
+    exit 1
+fi
+
+PYTHON_VERSION=3.0
+
+export PATH=$HOME/.pyenv/bin/pyenv:$PATH
+pytracer_owd=$(pwd)
+mydir=$(dirname $bs)
+cd $mydir
+. ./checkout_common.sh
+(cd $fulldir/.. && \
+     setup_version python-spark python-3.0-to-3.2 && \
+     setup_version python-xdis python-3.0-to-3.2 && \
+     setup_version python-filecache python-3.0-to-3.2 && \
+     setup_version pycolumnize python-3.0-to-3.5 && \
+     setup_version python-uncompyle6 python-3.0-to-3.2 \
+    )
+cd $owd
+rm -v */.python-version || true
+
+checkout_finish python-3.0-to-3.2
