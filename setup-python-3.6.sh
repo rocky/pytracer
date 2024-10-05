d0a37bebd2c839fcb51a0e1a63d0462d8e97bf6a
rocky <rocky@gnu.org>
2024-10-04 22:37:02 -0400

    Administrivia - go over setup-python

--- setup-python-3.6.sh
+++ setup-python-3.6.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# Check out 3.6-to-3.10 branch and dependent development branches
-s
+# Check out 3.6-to-3.10 branch and dependant development branches
+
 bs=${BASH_SOURCE[0]}
 if [[ $0 == $bs ]] ; then
     echo "This script should be *sourced* rather than run directly through bash"
     exit 1
 fi
 
-PYTHON_VERSION=3.6.15
+PYTHON_VERSION=3.6
 
 export PATH=$HOME/.pyenv/bin/pyenv:$PATH
-trepan3_owd=$(pwd)
+pytracer_owd=$(pwd)
 mydir=$(dirname $bs)
 cd $mydir
 . ./checkout_common.sh
