a2386a334c05d46be123214f0a580ffc7b0d4e4c
rocky <rocky@gnu.org>
2024-10-09 11:12:56 -0400

    Administrivia

--- admin-tools/setup-python-3.0.sh
+++ admin-tools/setup-python-3.0.sh
@@ -15,11 +15,11 @@ mydir=$(dirname $bs)
 cd $mydir
 . ./checkout_common.sh
 (cd $fulldir/.. && \
-     setup_version python-spark python-3.0-to-3.2 && \
-     setup_version python-xdis python-3.0-to-3.2 && \
-     setup_version python-filecache python-3.0-to-3.2 && \
-     setup_version pycolumnize python-3.0-to-3.5 && \
-     setup_version python-uncompyle6 python-3.0-to-3.2 \
+     setup_version python-spark python-3.0 && \
+     setup_version python-xdis python-3.0 && \
+     setup_version python-filecache python-3.0 && \
+     setup_version pycolumnize python-3.0 && \
+     setup_version python-uncompyle6 python-3.0 \
     )
 cd $pytracer_owd
 rm -v */.python-version 2>&/dev/null || true
